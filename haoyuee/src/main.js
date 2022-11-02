1c50d4182dbb39e6041f61988c094e3f6b46a1a2
daisyue <2498081681@qq.com>
2022-11-02 15:16:35 +0800

    day1

---
+++ haoyuee/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+// import Vue from "element-ui/lib";
+
+createApp(App).mount('#app')
+
+// Vue.directive('title', {
+//     inserted: function (el, binding) {
+//         document.title = el.dataset.title
+//     }
+// })
